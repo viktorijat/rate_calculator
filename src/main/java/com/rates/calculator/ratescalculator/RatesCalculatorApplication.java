6a1b8117fbfc2559eef8a461eeba1b663c6e2b6b
Viktorija Tosheva <viktorija.tosheva@tipico.com>
2019-02-01 13:30:46 +0100

    first commit

---
+++ src/main/java/com/rates/calculator/ratescalculator/RatesCalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.rates.calculator.ratescalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatesCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatesCalculatorApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/rates/calculator/ratescalculator/RatesCalculatorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rates.calculator.ratescalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RatesCalculatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
